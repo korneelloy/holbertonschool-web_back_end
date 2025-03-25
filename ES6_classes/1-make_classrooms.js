5057d6794062f98bbcdc7c275110814feecfe81d
korneel <korneel.loy@gmail.com>
2025-03-25 17:00:38 +0100

    js class ex 1

---
+++ ES6_classes/1-make_classrooms.js
@@ -0,0 +1,7 @@
+import ClassRoom from './0-classroom.js';
+
+export default function initializeRooms() {
+  const arrayOfTreeRooms = [new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)];
+
+  return (arrayOfTreeRooms);
+}
