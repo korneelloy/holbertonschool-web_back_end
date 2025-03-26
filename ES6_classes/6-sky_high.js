01bf569b30a32e20d1ea4aaaebb64be8d1d32d56
korneel <korneel.loy@gmail.com>
2025-03-26 11:47:40 +0100

    js class ex 6 typo

--- ES6_classes/6-sky_high.js
+++ ES6_classes/6-sky_high.js
@@ -18,6 +18,6 @@ export default class SkyHighBuilding extends Building {
   }
 
   evacuationWarningMessage() {
-    return (`Evacuate slowly the ${this.floors} floors.`);
+    return (`Evacuate slowly the ${this.floors} floors`);
   }
 }
