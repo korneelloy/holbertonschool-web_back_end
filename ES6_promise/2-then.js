70804e92504d03995f66166cb8587318e17d242e
korneel <korneel.loy@gmail.com>
2025-04-01 14:34:36 +0200

    ES6 promise ex 2 return

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,10 +1,6 @@
 export default function handleResponseFromAPI(promise) {
   console.log('Got a response from the API');
   promise
-    .then(() => {
-      return ({ status: 200, body: 'Success' });
-    })
-    .catch(() => {
-      return new Error();
-    });
+    .then(() => ({ status: 200, body: 'Success' }))
+    .catch(() => new Error());
 }
