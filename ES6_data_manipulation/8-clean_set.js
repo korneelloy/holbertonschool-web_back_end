5d56c9fe9d39b6600ee83acb6407c9d919c013c9
korneel <korneel.loy@gmail.com>
2025-04-01 11:13:58 +0200

    ES6 Data manip ex 9

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,9 +1,11 @@
 export default function cleanSet(theSet, startString) {
   let theString = '';
   let updatedElement = '';
+
   if (startString === '') {
     return theString;
   }
+
   for (const element of theSet) {
     if (element.indexOf(startString) === 0) {
       if (theString === '') {
