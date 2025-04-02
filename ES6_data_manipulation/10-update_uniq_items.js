d92de7ab5ebcbab2d78e83872cb0b2ed711b1eca
korneel <korneel.loy@gmail.com>
2025-04-02 15:41:00 +0200

    ES6 Data manip ex 10 error handling correction

--- ES6_data_manipulation/10-update_uniq_items.js
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -1,4 +1,7 @@
 export default function updateUniqueItems(groceriesMap) {
+  if (!(groceriesMap instanceof Map)) {
+    throw new Error('Cannot process');
+  }
   for (const [key, value] of groceriesMap) {
     if (value === 1) {
       groceriesMap.set(key, 100);
