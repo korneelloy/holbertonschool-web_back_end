32a531cc3b75dada23e8cc8c96d932e7a562a353
korneel <korneel.loy@gmail.com>
2025-04-21 16:56:23 +0200

    python var annotations ex0

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+def add(a: float, b: float) -> float:
+    """ type-annotated function for sum of and b
+    argv :  float: a
+            float : b
+    Returns: float
+    """
+    return (a + b)
