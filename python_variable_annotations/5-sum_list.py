1fe3a831179551390f377403512c415db0ae5db9
korneel <korneel.loy@gmail.com>
2025-04-21 18:19:57 +0200

    python var annoattaions ex 5 typing

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """ module returning sum of list of floats"""
+from typing import List
 
 
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     """type-annotated function
     argv: list of floats
     returns  sum as a float."""
