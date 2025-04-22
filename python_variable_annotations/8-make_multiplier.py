2a40fe828dbcab61f40bd699cf484177fc406ed5
korneel <korneel.loy@gmail.com>
2025-04-22 09:33:48 +0200

    python ex 8 correction

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """ module returning callable function with multiplier"""
-from collections.abc import Callable
+from typing import Callable
 
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
