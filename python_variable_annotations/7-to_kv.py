9ae15b6b5881c80ea81c8cef0577cab86fae1c5a
korneel <korneel.loy@gmail.com>
2025-04-22 09:32:19 +0200

    python ex 7 correction

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 """ module returning tupple"""
 import math
+from typing import Union, Tuple
 
 
-def to_kv(k: str, v: int | float) -> tuple[str, float]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """type-annotated function
     argv:   k: string
             v: int or float
