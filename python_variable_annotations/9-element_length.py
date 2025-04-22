38027c77b61df31cb87bef1bf8e3a7fe5393c568
korneel <korneel.loy@gmail.com>
2025-04-22 09:39:34 +0200

    python ex 9 correction

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """ module returning callable function with multiplier"""
-from collections.abc import Iterable, Sequence
+from typing import Iterable, Sequence, List, Tuple
 
 
-def element_length(lst: Iterable[Sequence]) -> list[tuple[int]]:
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """type-annotated function
     argv:   multiplier : float
     returns lambda function multiplying any float"""
