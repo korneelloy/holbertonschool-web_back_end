ce94c0ffb4279226488fe1b392db94dc39bf9b4e
korneel <korneel.loy@gmail.com>
2025-04-28 15:56:42 +0200

    python async comprehension ex 0

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+coroutine without arguments that loops 10 times,
+each time asynchronously wait 1 second,
+then yield a random number between 0 and 10.
+Use the random module.
+"""
+
+import random
+import asyncio
+
+
+async def async_generator():
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield (random.uniform(0, 10))
