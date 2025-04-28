a2d826910882afc66f430d1f8721f3d35f7c351d
korneel <korneel.loy@gmail.com>
2025-04-28 15:59:20 +0200

    python async comprehension ex 0 comment

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -11,6 +11,7 @@ import asyncio
 
 
 async def async_generator():
+    """asynchronous generator of 10 random numbers"""
     for _ in range(10):
         await asyncio.sleep(1)
         yield (random.uniform(0, 10))
