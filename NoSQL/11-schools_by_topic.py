652afee102fad3c83820255bfcde16fb65e1f61b
korneel <korneel.loy@gmail.com>
2025-04-29 17:41:17 +0200

    nosql correction 11

--- NoSQL/11-schools_by_topic.py
+++ NoSQL/11-schools_by_topic.py
@@ -13,4 +13,11 @@ def schools_by_topic(mongo_collection: Collection, topic: str) -> List[str]:
         topic: string
     Returns: a list
     """
-    return (mongo_collection.find({'topics': topic}))
+    list_schools = []
+    schools = mongo_collection.find()
+    for school in schools:
+        if 'topics' in school and school['topics'] is not None:
+            for t in school['topics']:
+                if t == topic:
+                    list_schools.append(school)
+    return list_schools
