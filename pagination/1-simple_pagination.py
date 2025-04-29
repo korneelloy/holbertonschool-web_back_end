3ee82a126a3af80e494869c0c008d815fc321d2b
korneel <korneel.loy@gmail.com>
2025-04-29 10:37:35 +0200

    Python pagination ex 1

---
+++ pagination/1-simple_pagination.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+
+"""
+returning list based upon start page and page sze
+"""
+
+import csv
+import math
+from typing import List
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """
+    function returning index of elements based upon start page and page size
+    argv:
+        int : page
+        int: page_size
+    Returns: tuple of start and end index
+    """
+    return (((page - 1) * page_size), (page * page_size))
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """
+        returning list based upon start page and page sze
+        """
+        assert isinstance(page, int), "page must be an integer"
+        assert isinstance(page_size, int), "page_size must be an integer"
+        assert (page > 0), "page must be greater than 0"
+        assert (page_size > 0), "page_size must be greater than 0"
+        index = index_range(page, page_size)
+        data = self.dataset()
+        list = []
+        for row in data[index[0]:index[1]]:
+            if row is not None:
+                list.append(row)
+        return list
