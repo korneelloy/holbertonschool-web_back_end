d1ce8dccf08a0817373812f58a7c35aa8c2a17c8
korneel <korneel.loy@gmail.com>
2025-04-30 10:16:36 +0200

    nosql ex 12 if name == main added

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -4,27 +4,28 @@ Python script that provides some stats about Nginx logs stored in MongoDB
 """
 from pymongo import MongoClient
 
-client = MongoClient()
-db = client.logs
-collection = db.nginx
+if __name__ == "__main__":
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    db = client.logs
+    collection = db.nginx
 
-total_count = collection.count_documents({})
-print(f"{total_count} logs\nMethods:")
+    total_count = collection.count_documents({})
+    print(f"{total_count} logs\nMethods:")
 
-get_count = collection.count_documents({"method": "GET"})
-print(f"\tmethod GET: {get_count}")
+    get_count = collection.count_documents({"method": "GET"})
+    print(f"\tmethod GET: {get_count}")
 
-post_count = collection.count_documents({"method": "POST"})
-print(f"\tmethod POST: {post_count}")
+    post_count = collection.count_documents({"method": "POST"})
+    print(f"\tmethod POST: {post_count}")
 
-put_count = collection.count_documents({"method": "PUT"})
-print(f"\tmethod PUT: {put_count}")
+    put_count = collection.count_documents({"method": "PUT"})
+    print(f"\tmethod PUT: {put_count}")
 
-patch_count = collection.count_documents({"method": "PATCH"})
-print(f"\tmethod PATCH: {patch_count}")
+    patch_count = collection.count_documents({"method": "PATCH"})
+    print(f"\tmethod PATCH: {patch_count}")
 
-delete_count = collection.count_documents({"method": "DELETE"})
-print(f"\tmethod DELETE: {delete_count}")
+    delete_count = collection.count_documents({"method": "DELETE"})
+    print(f"\tmethod DELETE: {delete_count}")
 
-status_count = collection.count_documents({"path": "/status"})
-print(f"{status_count} status check")
+    status_count = collection.count_documents({"path": "/status"})
+    print(f"{status_count} status check")
