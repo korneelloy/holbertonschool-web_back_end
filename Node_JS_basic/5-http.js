92cd35fc625769b78068f5779e1238b65e20cb58
korneel <korneel.loy@gmail.com>
2025-05-22 16:17:45 +0200

    nodejs ex 5 db as argv

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,16 +1,18 @@
 const http = require('http');
 const countStudents = require('./3-read_file_async');
 
+const database = process.argv[2];
+
 const app = http.createServer((req, res) => {
   if (req.url === '/') {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello Holberton School!');
   } else if (req.url === '/students') {
-    countStudents('database.csv')
+    countStudents(database)
       .then(({ total, fields }) => {
-        let text = `Number of students: ${total}\n`;
-        for (const field in fields) {
-          text += `Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}\n`;
+        const text = `Number of students: ${total}\n`;
+        for (const [field] of Object.entries(fields)) {
+          console.log(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`);
         }
         res.writeHead(200, { 'Content-Type': 'text/plain' });
         res.end(`This is the list of our students\n${text}`);
